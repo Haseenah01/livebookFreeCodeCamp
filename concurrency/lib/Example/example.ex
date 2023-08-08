71bed140662ed9cb22e5e01b2cf6bcde1aec509d
Haseenah01 <haseenahsami27@gmail.com>
2023-08-08 11:42:52 +0200

    concurrency example added

--- concurrency/lib/Example/example.ex
+++ concurrency/lib/Example/example.ex
@@ -3,4 +3,12 @@ defmodule Example.Example do
   def add(a, b) do
     IO.puts(a + b)
   end
+
+  def listen do
+    receive do
+      {:ok, "hello"} -> IO.puts("World")
+    end
+
+    listen()
+  end
 end
